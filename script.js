db124c064278712cd6d3642a32fe5c1a683449cf
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-06-17 22:00:25 +0200

    Create the food

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ snake[0] = {
 };
 
 let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
+};
 
 function createBackGround() {
   context.fillStyle = "lightgreen";
@@ -20,6 +24,11 @@ function createSnake() {
     context.fillStyle = "green";
     context.fillRect(snake[slide].x, snake[slide].y, box, box);
   }
+}
+
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -39,6 +48,7 @@ function startGame() {
 
   createBackGround();
   createSnake();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
