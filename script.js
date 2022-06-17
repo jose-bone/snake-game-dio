9bbb656d315e1c11a97141b4c20e262c68936748
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-06-17 21:04:27 +0200

    Define snake move

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ snake[0] = {
   y: 8 * box,
 };
 
+let direction = "right";
+
 function createBackGround() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -20,5 +22,26 @@ function createSnake() {
   }
 }
 
-createBackGround();
-createSnake();
+function startGame() {
+  createBackGround();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
